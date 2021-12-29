cf44bba14d9e4f9be1b6cebc66db361c8d7cb029
Neima Inslicht <neima924@gmail.com>
2021-12-28 23:24:46 -0500

    first commit

---
+++ my_mem.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void mem_init(unsigned char *my_memory, unsigned int my_mem_size)
+{
+
+}
+
+void *my_malloc(unsigned size)
+{
+
+}
+
+void my_free(void *mem_pointer)
+{
+
+}
+
+void mem_get_stats(mem_stats_ptr mem_stats_ptr)
+{
+
+}
