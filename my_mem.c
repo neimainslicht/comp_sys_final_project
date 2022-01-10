1a00e74151291fc7af9256612ed92d52e972e303
Neima Inslicht <inslicht@mail.yu.edu>
2022-01-10 17:10:39 -0500

    made global_size a global variable, combined insert and delete functions, added test_merge

--- my_mem.c
+++ my_mem.c
@@ -5,192 +5,203 @@
 #include <errno.h>
 #include <stdint.h>
 
+//global variable for memory size
+unsigned int global_mem_size = 0;
+
 
 //used and free lists are implemented as linked lists
 struct node 
 {
-   int size; //pointer to size of block
-   unsigned int global_size; //pointer to size of the entire memory
-   unsigned char location; //pointer to location
+   int size; //size of block
+   void *location; //location of head of block in memory
    struct node *next;
 };
 
 struct node *used_head = NULL; //head pointer to used list
 struct node *free_head = NULL; //head pointer to free list
 
-//change this to insert by location
-//insert node in order of location in memory
-void insert_used(unsigned char location, int size, int global_size) 
+void printlist(char list_type)
 {
-   //create a node
-   struct node *block = (struct node*) malloc(sizeof(struct node));
-	
-   block->location = location;
-   block->size = size;
-   block->global_size = global_size;
+    struct node* cur;
+    if(list_type == 'f')
+    {
+        //start from the first node
+        cur = free_head;      
+    }
+    else if (list_type == 'u')
+    {
+        //start from the first node
+        cur = used_head;
+    }
+    else
+    {
+        fprintf(stderr, "error: not valid list type\n");
+        return;
+    }
 
-   //start from the first node
-   struct node* cur = used_head;
-   struct node* prev = (struct node*) malloc(sizeof(struct node));
-	
-   //if list is empty, assign block as the head
-   if(used_head == NULL) 
-   {
-      //point block->next to NULL
-      block->next = NULL;
-	
-      //point head to new first node
-      used_head = block;
-   }
-
-   //loop through the list
-   else
-   {
-      while(cur != NULL && cur->location < location) 
-      {
-         //set prev to cur
-         prev = cur;
-         //move to next node
-         cur = cur->next;
-      }
-      //insert block
-      prev->next = block;
-      block->next = cur;
-   }
+    while (cur != NULL)
+    {
+        printf("%p ", cur->location);
+        cur = cur->next;
+    }
+    printf("\n");
+    
 }
 
+
 //insert node in order of location in memory
-void insert_free(unsigned char location, int size, int global_size) 
+//if list type = 'f', insert into free, if list type = 'u', insert into used
+void insert(void *location, int size, char list_type) 
 {
-   //create a node
-   struct node *block = (struct node*) malloc(sizeof(struct node));
+    //create a node
+    struct node *block = (struct node*) malloc(sizeof(struct node));
 	
-   block->location = location;
-   block->size = size;
-   block->global_size = global_size;
+    block->location = location;
+    block->size = size;
+    
+    struct node* cur;
+    if(list_type == 'f')
+    {
+        //start from the first node
+        cur = free_head;      
+    }
+    else if (list_type == 'u')
+    {
+        //start from the first node
+        cur = used_head;
+    }
+    else
+    {
+        fprintf(stderr, "error: not valid list type\n");
+        return;
+    }
+    
+    struct node* prev = NULL;
 
-   //start from the first node
-   struct node* cur = free_head;
-   struct node* prev = (struct node*) malloc(sizeof(struct node));
-	
-   //if list is empty, assign block as the head
-   if(free_head == NULL) 
-   {
-      //point block->next to NULL
-      block->next = NULL;
-	
-      //point head to new first node
-      free_head = block;
-   }
+	//if list is empty, assign block as the head
+    if(cur == NULL) 
+    {
+        //point block->next to NULL
+        block->next = NULL;
 
-   //loop through the list
-   else
-   {
-      while(cur != NULL && cur->location < location) 
-      {
-         //set prev to cur
-         prev = cur;
-         //move to next node
-         cur = cur->next;
-      }
-      //insert block
-      prev->next = block;
-      block->next = cur;
-   }
-       
-
+        //point head to new first node
+        if (list_type == 'f')
+        {
+            free_head = block;
+        }
+        else
+        {
+            used_head = block;
+        }
+        
+    }
+    //loop through the list
+    else
+    {
+        while(cur != NULL && cur->location < block->location) 
+        {
+            //set prev to cur
+            prev = cur;
+            //move to next node
+            cur = cur->next;
+        }
+        //insert block
+        //if the list has one node
+        if (prev == NULL)
+        {
+            if (list_type == 'f')
+            {
+                free_head = block;
+                block->next = cur;
+            }
+            else
+            {
+                used_head = block;
+                block->next = cur;
+            }
+            
+        }
+        else
+        {
+            prev->next = block;
+            block->next = cur;
+        }
+    }
 }
+
 
 //delete a node from the used list with given memory location
-void delete_used(unsigned char location) 
+void delete(unsigned char *location, char list_type) 
 {
-   //start from the first node
-   struct node* cur = used_head;
-   struct node* prev = (struct node*) malloc(sizeof(struct node));
+    struct node* cur;
+    if(list_type == 'f')
+    {
+        //start from the first node
+        cur = free_head;      
+    }
+    else if (list_type == 'u')
+    {
+        //start from the first node
+        cur = used_head;
+    }
+    else
+    {
+        fprintf(stderr, "error: not valid list type\n");
+        return;
+    }
+    
+
+    struct node* prev = NULL;
 	
-   //if list is empty, return
-   if(used_head == NULL) 
-   {
-      return;
-   }
+    //if list is empty, return
+    if(cur == NULL) 
+    {
+        fprintf(stderr, "error: list is empty, nothing to delete\n");
+        return;
+    }
 
-   //loop through the list
-   while(cur != NULL && cur->location != location) 
-   {
+    //loop through the list
+    while(cur->location != location) 
+    {
+ 
+        //if it is the last node
+        if(cur->next == NULL) 
+        {
+            fprintf(stderr, "error: couldn't find node to delete\n");
+            return;
+        } 
+        else 
+        { 
+            //store reference to current node
+            prev = cur;
+            //move to next node
+            cur = cur->next;
+        }
+    }
 
-      //if it is the last node
-      if(cur->next == NULL) 
-      {
-         return;
-      } 
-      else 
-      {
-         //store reference to current node
-         prev = cur;
-         //move to next node
-         cur = cur->next;
-      }
-   }
-
-   //found a match, update the node
-   if(cur == used_head) 
-   {
-      //change head to point to next 
-      used_head = used_head->next;
-   } 
-   else 
-   {
-      //skip over the current node
-      prev->next = cur->next;
-   }    
+    //found a match, delete the node
+    //if only one node in list
+    if(cur == used_head || cur == free_head) 
+    {
+        if (list_type == 'f')
+        {
+            //change free head to point to next 
+            free_head = free_head->next;
+        }
+        else
+        {
+            //change used head to point to next 
+            used_head = used_head->next;
+        }
+    } 
+    else 
+    {
+        //skip over the current node
+        prev->next = cur->next;
+    }    
 
 }
 
-//delete a node from the free list with given memory location
-void delete_free(unsigned char location) 
-{
-
-   //start from the first node
-   struct node* cur = free_head;
-   struct node* prev = (struct node*) malloc(sizeof(struct node));
-	
-   //if list is empty
-   if(free_head == NULL) 
-   {
-      return;
-   }
-
-   //loop through the list
-   while(cur != NULL && cur->location != location) 
-   {
-
-      //if it is last node
-      if(cur->next == NULL) 
-      {
-         return;
-      } 
-      else 
-      {
-         //store reference to current node
-         prev = cur;
-         //move to next node
-         cur = cur->next;
-      }
-   }
-
-   //found a match, update the node
-   if(cur == free_head) 
-   {
-      //change head to point to next 
-      free_head = free_head->next;
-   } 
-   else 
-   {
-      //skip over the current node
-      prev->next = cur->next;
-   }    
-	
-}
 
 //merge free blocks that are adjacent in memory
 void merge_blocks(void)
@@ -215,6 +226,11 @@ void merge_blocks(void)
          }
          
       }
+
+      if (cur == NULL)
+      {
+          break;
+      }
       
       //store reference to current node
       prev = cur;
@@ -228,19 +244,15 @@ void merge_blocks(void)
 
 void mem_init(unsigned char *my_memory, unsigned int my_mem_size)
 {
-  insert_used(*my_memory, my_mem_size, my_mem_size); //initialize pointer to memory pool
-  insert_free(*my_memory, my_mem_size, my_mem_size); //initialize pointer to free list
+    global_mem_size = my_mem_size;
+    insert((void *) my_memory, my_mem_size, 'f'); //insert memory into free list
 
 }
 
 void *my_malloc(unsigned size)
 {
   //if you are trying to allocate too much memory, send an error
-  if(used_head == NULL)
-  {
-     used_head = (struct node*) malloc(sizeof(struct node));
-  }
-  else if((unsigned int) size > used_head->global_size)
+  if(size > global_mem_size)
   {
     fprintf(stderr, "error: not enough memory available\n");
     return NULL;
@@ -252,7 +264,7 @@ void *my_malloc(unsigned size)
   while(cur != NULL)
 
   {
-    //if we found a large enough block, allocate enough memory and save the rest in free list
+    //if we found a large enough block
     if (cur->size >= size)
 
     {
@@ -261,28 +273,31 @@ void *my_malloc(unsigned size)
 
       {
         //insert into used
-        insert_used(cur->location, size, cur->global_size);
+        insert(cur->location, size, 'u');
+        void *temp = cur->location;
 
         //reduce the amount of memory in the block we found in the free list by size
         cur->location = cur->location + size;
         cur->size = cur->size - size;
+        return temp;
       }
       
       //otherwise, just use the whole block
       else 
       {
         //insert into used
-        insert_used(cur->location, cur->size, cur->global_size);
+        insert(cur->location, cur->size, 'u');
 
         //delete from free
-        delete_free(cur->location);
-      }
-       
+        delete(cur->location, 'f');
+
+        return cur->location;
+      }   
     }
     
     cur = cur->next;
   }
-  
+   
 }
 
 void my_free(void *mem_pointer)
@@ -296,12 +311,11 @@ void my_free(void *mem_pointer)
   while(cur != NULL)
   {
     //if you found it, delete it from the used list and add it to the free list
-    if((intptr_t) mem_pointer == cur->location)//cast to intptr_t to store memory address
+    if(mem_pointer == cur->location)
     {
-       printf("mem_pointer: %ld, cur->location: %d\n", (intptr_t) mem_pointer, cur->location);
-       insert_free(cur->location, cur->size, cur->global_size);
-       delete_used(cur->location);
-       return;
+      insert(cur->location, cur->size, 'f');
+      delete(cur->location, 'u');
+      return;
     }
     cur = cur->next;
   }
@@ -316,93 +330,98 @@ void my_free(void *mem_pointer)
 }
 
 typedef struct  {
-  int num_blocks_used;
-  int num_blocks_free;
-  int smallest_block_free;
-  int smallest_block_used;
-  int largest_block_free;
-  int largest_block_used;
+    int num_blocks_used;
+    int num_blocks_free;
+    int smallest_block_free;
+    int smallest_block_used;
+    int largest_block_free;
+    int largest_block_used;
 } mem_stats_struct, *mem_stats_ptr;
 
 void mem_get_stats(mem_stats_ptr mem_stats_ptr)
 {
-   //get stats for free
-   struct node* free_cur = free_head;
-   int num_blocks_free = 0;
-   int smallest_size_free = free_head->global_size;
-   int largest_size_free = 0;
-   mem_stats_ptr->smallest_block_free = 0;
-   mem_stats_ptr->largest_block_free = 0;
+    //get stats for free
+    struct node* free_cur = free_head;
+    int num_blocks_free = 0;
+    mem_stats_ptr->smallest_block_free = global_mem_size;
+    mem_stats_ptr->largest_block_free = 0;
 
-   //loop through all the nodes in free
-   while (free_cur != NULL)
-   {
-      //find smallest free block
-      if (free_cur->size < smallest_size_free)
-      {
-         mem_stats_ptr->smallest_block_free = free_cur->location;
-      }
+    if (free_cur == NULL)
+    {
+        mem_stats_ptr->smallest_block_free = 0;
+    }
+
+    //loop through all the nodes in free
+    while (free_cur != NULL)
+    {
+        //find smallest free block
+        if (free_cur->size < mem_stats_ptr->smallest_block_free)
+        {
+            mem_stats_ptr->smallest_block_free = free_cur->size;
+        }
       
-      //find largest free block
-      else if (free_cur->size > largest_size_free)
-      {
-         mem_stats_ptr->largest_block_free = free_cur->location;
-      }
+        //find largest free block
+        if (free_cur->size > mem_stats_ptr->largest_block_free)
+        {
+            mem_stats_ptr->largest_block_free = free_cur->size;
+        }
       
-      //increment the number of blocks
-      num_blocks_free++;
-      free_cur = free_cur->next;
-   }
+        //increment the number of blocks
+        num_blocks_free++;
+        free_cur = free_cur->next;
+    }
    
-   mem_stats_ptr->num_blocks_free = num_blocks_free;
+    mem_stats_ptr->num_blocks_free = num_blocks_free;
 
-   /*-------------------------------------------------*/
-   //do the same for used list
-   struct node* used_cur = used_head;
-   int num_blocks_used = 0;
-   int smallest_size_used = used_head->global_size;
-   int largest_size_used = 0;
-   mem_stats_ptr->smallest_block_used = 0;
-   mem_stats_ptr->largest_block_used = 0;
+    /*-------------------------------------------------*/
+    //do the same for used list
+    struct node* used_cur = used_head;
+    int num_blocks_used = 0;
+    mem_stats_ptr->smallest_block_used = global_mem_size;
+    mem_stats_ptr->largest_block_used = 0;
 
-   //loop through all the nodes in used
-   while (used_cur != NULL)
-   {
-      //find smallest used block
-      if (used_cur->size < smallest_size_used)
-      {
-         mem_stats_ptr->smallest_block_used = used_cur->location;
-      }
+    if (used_cur == NULL)
+    {
+        mem_stats_ptr->smallest_block_used = 0;
+    }
+    
+    //loop through all the nodes in used
+    while (used_cur != NULL)
+    {
+        //find smallest used block
+        if (used_cur->size < mem_stats_ptr->smallest_block_used)
+        {
+            mem_stats_ptr->smallest_block_used = used_cur->size;
+        }
       
-      //find largest used block
-      else if (used_cur->size > largest_size_used)
-      {
-         mem_stats_ptr->largest_block_used = used_cur->location;
-      }
+        //find largest used block
+        if (used_cur->size > mem_stats_ptr->largest_block_used)
+        {
+            mem_stats_ptr->largest_block_used = used_cur->size;
+        }
       
-      //increment the number of blocks
-      num_blocks_used++;
-      used_cur = used_cur->next;
-   }
+        //increment the number of blocks
+        num_blocks_used++;
+        used_cur = used_cur->next;
+    }
    
-   mem_stats_ptr->num_blocks_used = num_blocks_used;
+    mem_stats_ptr->num_blocks_used = num_blocks_used;
 }
 
 void print_stats(char *prefix) {
-  mem_stats_struct mem_stats;
+    mem_stats_struct mem_stats;
 
-  mem_get_stats(&mem_stats);
-  printf("mem stats: %s: %d free blocks, %d used blocks, free blocks: smallest=%d largest=%d, used blocks: smallest=%d largest=%d\n",
-	 prefix,
-	 mem_stats.num_blocks_free,
-	 mem_stats.num_blocks_used,
-	 mem_stats.smallest_block_free,
-	 mem_stats.largest_block_free,
-	 mem_stats.smallest_block_used,
-	 mem_stats.largest_block_used);
+    mem_get_stats(&mem_stats);
+    printf("mem stats: %s: %d free blocks, %d used blocks, free blocks: smallest=%d largest=%d, used blocks: smallest=%d largest=%d\n",
+	prefix,
+	mem_stats.num_blocks_free,
+	mem_stats.num_blocks_used,
+	mem_stats.smallest_block_free,
+	mem_stats.largest_block_free,
+	mem_stats.smallest_block_used,
+	mem_stats.largest_block_used);
 }
 
-/*------TEST FUNCTIONS--------*/
 //try to malloc when all the memory is allocated
 void test_malloc_nothing()
 {
@@ -410,7 +429,7 @@ void test_malloc_nothing()
    unsigned char *global_memory = malloc(global_mem_size);
    unsigned int size = 50;
    my_malloc(size);
-   my_malloc(size);//if malloc returns an error, free is working
+   my_malloc(size);//if malloc returns an error, malloc is working
 }
 
 //try to free something that is not malloced
@@ -433,36 +452,56 @@ void test_malloc_more()
    unsigned char *ptr = my_malloc(size+50);//if malloc returns an error, malloc is working
 }
 
-int main(int argc, char **argv)
+//test to see if merge works
+void test_merge()
 {
-   test_free_nothing();
-   test_malloc_more();
-   test_malloc_nothing();
-  unsigned int global_mem_size = 1024 * 1024;
-  unsigned char *global_memory = malloc(global_mem_size);
+    //insert one block next to another
+    unsigned char *mem = malloc(50);
+    insert(mem, 50, 'f');
+    insert(mem+50, 50, 'f');
 
-  mem_init(global_memory, global_mem_size);
-  print_stats("init");
+    //print the list before and after merging to see if it works
+    printlist('f');
+    merge_blocks();
+    printlist('f');
 
-  unsigned char *ptr_array[10];
-  unsigned int sizes[] = {50, 20, 20, 20, 50, 0};
+    //deallocate the memory allocated
+    delete(mem, 'f');
+    delete(mem+50, 'f');
+    free(mem);
 
-  for (int i = 0; sizes[i] != 0; i++) {
-    char buf[1024];
-    ptr_array[i] = my_malloc(sizes[i]);
-    
-    sprintf(buf, "after iteration %d size %d", i, sizes[i]);
-    print_stats(buf);
-  }
-
-  my_free(ptr_array[1]);  print_stats("after free #1");
-
-  my_free(ptr_array[3]);  print_stats("after free #3");
-
-  my_free(ptr_array[2]);  print_stats("after free #2");
-
-  my_free(ptr_array[0]);  print_stats("after free #0");
-
-  my_free(ptr_array[4]);  print_stats("after free #4");
 }
 
+int main(int argc, char **argv)
+{
+    test_merge();
+    test_free_nothing();
+    test_malloc_more();
+    test_malloc_nothing();
+    unsigned int global_mem_size = 1024 * 1024;
+    unsigned char *global_memory = malloc(global_mem_size);
+
+    mem_init(global_memory, global_mem_size);
+    print_stats("init");
+
+    unsigned char *ptr_array[10];
+    unsigned int sizes[] = {50, 20, 20, 20, 50, 0};
+
+    for (int i = 0; sizes[i] != 0; i++) {
+        char buf[1024];
+        ptr_array[i] = my_malloc(sizes[i]);
+        
+        sprintf(buf, "after iteration %d size %d", i, sizes[i]);
+        print_stats(buf);
+    }
+
+    my_free(ptr_array[1]);  print_stats("after free #1");
+
+    my_free(ptr_array[3]);  print_stats("after free #3");
+
+    my_free(ptr_array[2]);  print_stats("after free #2");
+
+    my_free(ptr_array[0]);  print_stats("after free #0");
+
+    my_free(ptr_array[4]);  print_stats("after free #4");
+}
